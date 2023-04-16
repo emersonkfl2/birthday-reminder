cb40ab9fbcc15fe497d1e5ff2bdbcc5c8691813a
emersonkf2 <emersonkf2@protonmail.com>
2023-04-16 17:46:57 -0300

    Add the class to send emails

--- src/main/java/org/birthday/Main.java
+++ src/main/java/org/birthday/Main.java
@@ -4,15 +4,17 @@ import org.birthday.domain.Friend;
 import org.birthday.domain.FriendRepository;
 import org.birthday.infrastructure.FlatFileFriendRepository;
 import org.birthday.infrastructure.SQLiteFriendRepository;
+import org.birthday.service.EmailService;
 
 import java.nio.file.Path;
+import java.time.LocalDate;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         //Test JPA FriendRepository with SQLite
-        FriendRepository friendRepository = new SQLiteFriendRepository("birthdayReminderPU");
-        List<Friend> friends = friendRepository.findAll();
+        FriendRepository sqLiteRepository = new SQLiteFriendRepository("birthdayReminderPU");
+        List<Friend> friends = sqLiteRepository.findAll();
 
         System.out.println("\n------------Getting friends from SQLite------------");
         for (Friend friend : friends) {
@@ -29,6 +31,29 @@ public class Main {
             System.out.println("Friend: " + friend.getFirstName() + " " + friend.getLastName() +
                     ", Date of Birth: " + friend.getFormattedDateOfBirth() + ", Email: " + friend.getEmail());
         }
+
+
+        String fromEmail = "your-email@gmail.com";
+        String fromEmailPassword = "your-email-password";
+        EmailService emailService = new EmailService(fromEmail, fromEmailPassword);
+
+        for (Friend friend : friends) {
+            if (isBirthdayToday(friend.getDateOfBirth())) {
+                String subject = "Happy Birthday!";
+                String body = "Dear " + friend.getFirstName() + ",\n\nHappy Birthday!\n\nBest wishes,\nBirthday Reminder";
+                emailService.sendEmail(friend.getEmail(), subject, body);
+            }
+        }
+
+    }
+    private static boolean isBirthdayToday(LocalDate dateOfBirth) {
+        LocalDate today = LocalDate.now();
+        if (today.getMonth() == dateOfBirth.getMonth() && today.getDayOfMonth() == dateOfBirth.getDayOfMonth()) {
+            return true;
+        } else
+            return today.getMonthValue() == 2 && today.getDayOfMonth() == 28 &&
+                    dateOfBirth.getMonthValue() == 2 && dateOfBirth.getDayOfMonth() == 29 &&
+                    !today.isLeapYear();
     }
 }
 
---
+++ src/main/java/org/birthday/service/EmailService.java
@@ -0,0 +1,47 @@
+package org.birthday.service;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.util.Properties;
+
+public class EmailService {
+
+    private final String fromEmail;
+    private final String fromEmailPassword;
+
+    public EmailService(String fromEmail, String fromEmailPassword) {
+        this.fromEmail = fromEmail;
+        this.fromEmailPassword = fromEmailPassword;
+    }
+
+    public void sendEmail(String toEmail, String subject, String body) {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        Authenticator authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, fromEmailPassword);
+            }
+        };
+
+        Session session = Session.getInstance(properties, authenticator);
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(fromEmail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
